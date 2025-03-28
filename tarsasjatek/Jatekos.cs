39d4d0f33e6558099af6dda4c5bf5ee033700f3a
kormonditamas2222 <t.kormond2006@gmail.com>
2025-03-28 08:28:08 +0100

    jatekos class

---
+++ tarsasjatek/Jatekos.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tarsasjatek
+{
+    
+    internal class Jatekos
+    {
+        static Random random = new Random();
+        string name;
+        int pos;
+
+        public Jatekos(string name)
+        {
+            this.name = name;
+            this.pos = 0;
+        }
+
+        public string Name { get => name; set => name = value; }
+        public int Pos { get => pos; set => pos = value; }
+
+        public override string? ToString()
+        {
+            return $"Jelenlegi pozíció {name} játékosnak: {pos}";
+        }
+        public void Dobas()
+        {
+            pos += random.Next(1, 7);
+        }
+    }
+}
